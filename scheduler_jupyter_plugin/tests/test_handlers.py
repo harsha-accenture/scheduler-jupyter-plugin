e6d9284db1d0059401e23c21b55bbdba8ee2b8bd
risha-vijayvargiya-22 <risha.vijayvargiya@accenture.com>
2025-01-27 13:13:08 +0000

    renamed models folder

--- scheduler_jupyter_plugin/tests/test_handlers.py
+++ scheduler_jupyter_plugin/tests/test_handlers.py
@@ -20,4 +20,3 @@ async def test_get_default_settings(jp_fetch):
     assert response.code == 200
     payload = json.loads(response.body)
     assert "log_path" in payload
-    assert payload["log_path"] is ""
