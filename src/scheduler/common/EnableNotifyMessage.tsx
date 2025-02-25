71f6b3a6157e67042a33720ed707a72dbf0e59eb
vysakhviswama <vysakhma@google.com>
2025-02-25 15:27:31 +0000

    Lint issue fixed

--- src/scheduler/common/EnableNotifyMessage.tsx
+++ src/scheduler/common/EnableNotifyMessage.tsx
@@ -19,6 +19,7 @@ import React from 'react';
 
 const EnableNotifyMessage = ({ message }: { message: string }): JSX.Element => {
   const pattern =
+    // eslint-disable-next-line no-use-before-define
     /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g; // REGX to extract URL from string
   const url: any = message.match(pattern);
   const beforeLink = message.split('. ')[0] || '';
