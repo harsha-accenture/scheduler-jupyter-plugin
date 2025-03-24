dbf9a33acec6ca9c77de0c2d303e96ab102a1604
risha-vijayvargiya-22 <risha.vijayvargiya@accenture.com>
2025-03-24 12:54:15 +0000

    resolved pr comment

--- src/services/SchedulerServices.ts
+++ src/services/SchedulerServices.ts
@@ -630,7 +630,7 @@ export class SchedulerService {
         if (jsonstr) {
           const errorObject = JSON.parse(jsonstr);
           toast.error(
-            `Failed to fetch schedule list : ${errorObject.error.message}`,
+            `Failed to fetch scheduler list : ${errorObject.error.message}`,
             {
               ...toastifyCustomStyle,
               toastId: 'dagListError'
@@ -648,7 +648,7 @@ export class SchedulerService {
         LOG_LEVEL.ERROR
       );
       if (!toast.isActive('dagListError')) {
-        toast.error(`Failed to fetch dag list : ${error}`, {
+        toast.error(`Failed to fetch scheduler list : ${error}`, {
           ...toastifyCustomStyle,
           toastId: 'dagListError'
         });
