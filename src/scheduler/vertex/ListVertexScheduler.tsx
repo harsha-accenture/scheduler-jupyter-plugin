fde918207a94fee24911de08653d38c53be9c058
nirupama-dev <vnirupama@google.com>
2025-04-02 07:40:01 +0000

    Next Run ststus to be set as blank if  job is alreday completed

--- src/scheduler/vertex/ListVertexScheduler.tsx
+++ src/scheduler/vertex/ListVertexScheduler.tsx
@@ -26,6 +26,7 @@ import DeletePopup from '../../utils/DeletePopup';
 import { scheduleMode } from '../../utils/Const';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { RegionDropdown } from '../../controls/RegionDropdown';
+import { iconDash } from '../../utils/Icons';
 import { authApi } from '../../utils/Config';
 import {
   iconActive,
@@ -524,14 +525,25 @@ function ListVertexScheduler({
         </td>
       );
     } else if (cell.column.Header === 'Next Run Date') {
-      return (
-        <td
-          {...cell.getCellProps()}
-          className="clusters-table-data table-cell-overflow"
-        >
-          {dayjs(cell.row.original.nextRunTime).format('lll')}
-        </td>
-      );
+      if (cell.row.original.status === 'COMPLETED') {
+        return (
+          <td
+            {...cell.getCellProps()}
+            className="clusters-table-data table-cell-overflow"
+          >
+            <iconDash.react tag="div" />
+          </td>
+        );
+      } else {
+        return (
+          <td
+            {...cell.getCellProps()}
+            className="clusters-table-data table-cell-overflow"
+          >
+            {dayjs(cell.row.original.nextRunTime).format('lll')}
+          </td>
+        );
+      }
     } else if (cell.column.Header === 'Latest Execution Jobs') {
       return (
         <td
@@ -547,7 +559,7 @@ function ListVertexScheduler({
                       {job === 'JOB_STATE_SUCCEEDED' ? (
                         <iconSuccess.react
                           tag="div"
-                          title="Done !"
+                          title={job}
                           className="icon-white logo-alignment-style success_icon icon-size icon-completed"
                         />
                       ) : job === 'JOB_STATE_FAILED' ||
@@ -555,11 +567,13 @@ function ListVertexScheduler({
                         job === 'JOB_STATE_PARTIALLY_SUCCEEDED' ? (
                         <iconFailed.react
                           tag="div"
+                          title={job}
                           className="logo-alignment-style success_icon icon-size icon-completed"
                         />
                       ) : (
                         <iconPending.react
                           tag="div"
+                          title={job}
                           className="logo-alignment-style success_icon icon-size icon-completed"
                         />
                       )}
@@ -570,6 +584,7 @@ function ListVertexScheduler({
             ) : (
               <iconPending.react
                 tag="div"
+                title="No Job State Found"
                 className="logo-alignment-style success_icon icon-size icon-completed"
               />
             )
@@ -701,7 +716,7 @@ function ListVertexScheduler({
     }
   };
 
-  const openEditDagNotebookFile = async () => {
+  const openEditVertexNotebookFile = async () => {
     const filePath = inputNotebookFilePath.replace('gs://', 'gs:');
     const openNotebookFile = await app.commands.execute('docmanager:open', {
       path: filePath
@@ -714,7 +729,7 @@ function ListVertexScheduler({
 
   useEffect(() => {
     if (inputNotebookFilePath !== '') {
-      openEditDagNotebookFile();
+      openEditVertexNotebookFile();
     }
   }, [inputNotebookFilePath]);
 
